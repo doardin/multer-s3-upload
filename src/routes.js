e36d85b4833d85434606631e4ce172a94c9444a0
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-01-27 22:02:00 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+const routes = require("express").Router();
+const multer = require("multer");
+const multerConfig = require("./config/multer");
+
+routes.get("/posts", async (req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
+});
+
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  return res.json(req.file);
+});
+
+routes.delete("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
+});
+
+module.exports = routes;
